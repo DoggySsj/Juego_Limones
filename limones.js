37a83a1460090580ed887756364f160aaf9a635e
DoggySsj <ian2008doggy@gmail.com>
2026-09-17 21:22:51 -0500

    agregar dibujarSuelo y dibujarPersonaje

--- limones.js
+++ limones.js
@@ -1,1 +1,22 @@
-let ctx=document.getElementById("areaJuego").getContext("2d");
+let canvas=document.getElementById("areaJuego");
+let ctx=canvas.getContext("2d");
+const ALTURA_SUELO=40;
+const ALTURA_PERSONAJE=60;
+const ANCHO_PERSONAJE=60;
+
+function dibujarSuelo(){
+    ctx.fillStyle="blue";
+    ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO);
+}
+
+function dibujarPersonaje(){
+    ctx.fillStyle="white";
+    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+
+}
+
+function iniciar(){
+    dibujarSuelo();
+    dibujarPersonaje();
+
+}
