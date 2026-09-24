caf4944b9d916c6baaf3a7972c70efdbe6c78292
DoggySsj <ian2008doggy@gmail.com>
2026-09-23 20:24:44 -0500

    Se agrego bajar limon

--- limones.js
+++ limones.js
@@ -5,6 +5,13 @@ const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
 let personajeX=canvas.width/2;
 
+let limonX=canvas.width/2;
+let limonY=6;
+
+const ANCHO_LIMON=20;
+const ALTURA_LIMON=20;
+
+
 function dibujarSuelo(){
     ctx.fillStyle="blue";
     ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO);
@@ -19,6 +26,7 @@ function dibujarPersonaje(){
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 
 }
 
@@ -36,9 +44,23 @@ function actualizarCanva(){
     limpiarCanvas();
     dibujarSuelo()
     dibujarPersonaje();
+    dibujarLimon();
 }
 
 
 function limpiarCanvas(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+
+function dibujarLimon(){
+    ctx.fillStyle="green";
+    ctx.fillRect(limonX,limonY,ANCHO_LIMON,ALTURA_LIMON);
+}
+
+
+
+function bajarLimon(){
+    limonY=limonY+10
+    actualizarCanva();
 }
